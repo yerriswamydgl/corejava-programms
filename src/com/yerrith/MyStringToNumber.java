c9aee3d08488c931aaed1f5939d5e851914c0d0f
yerrith <yerrith@yerrith-PC>
2018-12-16 18:22:08 +0530

    JABI ADDED THIS CLASS

---
+++ src/com/yerrith/MyStringToNumber.java
@@ -0,0 +1,36 @@
+package com.yerrith;
+
+/**
+ * 
+ */
+/**
+ * @Author---Yerrith
+ */
+/**
+ * @author yerrith
+ *
+ */
+public class MyStringToNumber {
+ 
+    public static int convert_String_To_Number(String numStr){
+         
+        char ch[] = numStr.toCharArray();
+        //System.out.println();
+        int sum = 0;
+        //get ascii value for zero
+        int zeroAscii = (int)'0';
+        System.out.println(zeroAscii);
+        for(char c:ch){
+            int tmpAscii = (int)c;
+            sum = (sum*10)+(tmpAscii-zeroAscii);
+        }
+        return sum;
+    }
+     
+    public static void main(String a[]){
+         
+        System.out.println("\"3256\" == "+convert_String_To_Number("3256"));
+        System.out.println("\"76289\" == "+convert_String_To_Number("76289"));
+        System.out.println("\"90087\" == "+convert_String_To_Number("90087"));
+    }
+}
